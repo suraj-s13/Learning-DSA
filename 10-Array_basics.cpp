fe28160d4f785a7ae6e93ff51c9a1e0429b8e750
Suraj <13surajsingh01@gmail.com>
2022-05-10 22:49:17 +0530

    Basic of array

---
+++ 10-Array_basics.cpp
@@ -0,0 +1,22 @@
+//Declaration of array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A[5];
+    //all element will be random
+    int B[5] = {2,4,6,8,10};
+
+   int C[5] = {2,4};
+   for (int i = 0; i < 5; i++)
+   {
+       cout<<C[i]<<" ";
+   }
+   cout<<endl;
+
+    cout<<2[B]<<endl;
+    return 0;
+    
+}
